37d6f0235b80bf39294e0d05870d7a6157c31ab6
Phamminhtrikp <tripham.210504@gmail.com>
2025-03-22 16:20:22 +0700

    Generate Model fillable and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -20,7 +21,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'email_verified_at',
         'password',
+        'avatar',
+        'is_admin',
     ];
 
     /**
@@ -45,4 +49,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function groups() {
+        return $this->belongsToMany(Group::class, 'group_users');
+    }
+    
 }
